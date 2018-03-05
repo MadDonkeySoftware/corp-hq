079afe23e714de30d6477efc291695d804474df5
Frito <fritogotlayed@gmail.com>
2018-03-04 19:41:05 -0600

    CHQ-8 Created CORS example

--- app/ui/src/components/HelloWorld.vue
+++ app/ui/src/components/HelloWorld.vue
@@ -2,93 +2,68 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    <div>
+      <button @click="query" :disabled="quering">Query</button>
+    </div>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
+        <a href="https://vuejs.org" target="_blank"> Core Docs </a>
       </li>
       <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
+        <a href="https://forum.vuejs.org" target="_blank"> Forum </a>
       </li>
       <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
+        <a href="https://chat.vuejs.org" target="_blank"> Community Chat </a>
       </li>
       <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
+        <a href="https://twitter.com/vuejs" target="_blank"> Twitter </a>
       </li>
       <br>
       <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank"> Docs for This Template </a>
       </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
       <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
+        <a href="http://router.vuejs.org/" target="_blank"> vue-router </a>
       </li>
       <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
+        <a href="http://vuex.vuejs.org/" target="_blank"> vuex </a>
       </li>
       <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
+        <a href="http://vue-loader.vuejs.org/" target="_blank"> vue-loader </a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank"> awesome-vue </a>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      quering: false
+    }
+  },
+  methods: {
+    query () {
+      this.quering = true
+      axios.get('http://127.0.0.1:5000/api/values')
+        .then(response => {
+          // Json response
+          this.msg = 'Welcome to your Vue.js App. ' + response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+      this.quering = false
     }
   }
 }
