a90ab2c8819f37b602f9e0ea23e8f9bc14a6ab98
Frito <fritogotlayed@gmail.com>
2018-05-15 22:25:40 -0500

    CHQ-33 Localizing navigation bar

--- app/ui/src/i18n/en.js
+++ app/ui/src/i18n/en.js
@@ -1,10 +1,13 @@
 export default {
   'agreeWithTermsAndConditions': 'I agree to the terms and conditions.',
-  'reviewTermsAndConditions': 'Review them here.',
   'confirmPassword': 'Confirm Password',
   'email': 'Email',
-  'pleaseCorrectErrors': 'Please correct the following error(s):',
+  'logIn': 'Log In',
+  'logOut': 'Log Out',
   'password': 'Password',
+  'pleaseCorrectErrors': 'Please correct the following error(s):',
+  'register': 'Register',
+  'reviewTermsAndConditions': 'Review them here.',
   'submit': 'Submit',
   'username': 'Username'
 }
