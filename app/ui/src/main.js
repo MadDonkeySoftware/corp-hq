c51921fd398928cc139ed2d6291633e5f84152da
Frito <fritogotlayed@gmail.com>
2018-05-27 20:32:22 -0500

    CHQ-103 Moved token handling from event-based system to vuex-based system.

--- app/ui/src/main.js
+++ app/ui/src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
+import store from './store'
 import App from './App'
 import router from './router'
 import vuexI18n from 'vuex-i18n'
 import translationsEn from './i18n/en'
-import constants from '@/constants'
 
 // Set up a global mechanism to pass events
+/*
 window.Event = new class {
   constructor () {
     this.vue = new Vue()
@@ -22,11 +22,10 @@ window.Event = new class {
     this.vue.$on(event, callback)
   }
 }()
+*/
 
 Vue.config.productionTip = false
 
-Vue.use(Vuex)
-const store = new Vuex.Store()
 Vue.use(vuexI18n.plugin, store)
 
 Vue.i18n.add('en', translationsEn)
@@ -39,14 +38,5 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>',
-  mounted () {
-    Event.listen(constants.authTokenUpdated, function (value) {
-      if (value == null || value === undefined) {
-        localStorage.removeItem('token')
-      } else {
-        localStorage.setItem('token', value)
-      }
-    })
-  }
+  template: '<App/>'
 })
